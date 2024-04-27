cad0e9baace066a754b26dc819df16e7d6d3c60c
Mateusz Rojek <mateuszr2047@gmail.com>
2024-04-27 15:46:03 +0200

    Utworzenie projektu

---
+++ src/test/java/pl/mateusz/example/springsecurityproject/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.example.springsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
